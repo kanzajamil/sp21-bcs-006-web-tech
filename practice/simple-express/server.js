3fd4aa119983f514f0ff1c42b3ba3c6cf9f4a241
kanzajamil <kanzajamil@users.noreply.github.com>
2023-10-09 00:32:58 +0500

    Lab Task One (Jquery Validate plugin)

---
+++ practice/simple-express/server.js
@@ -0,0 +1,17 @@
+let express = require("express");
+let app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+// app.get("/", function (req, res) {
+//   res.send("<h1>Home Page</h1>");
+// });
+app.get("/contact-us", function (req, res) {
+  res.render("contact-us");
+});
+app.get("/", function (req, res) {
+  res.render("landing-page");
+});
+
+app.listen(5000);
+
